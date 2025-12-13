69b811fd79ab0f1e03307049b2b07e19c76625d9
Alfonso Harita <alharita@Alfonsos-MacBook-Pro.local>
2025-12-13 13:05:16 -0700

    fix(config): update baseUrl to /engineer-to-leader/ to match repo name

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,12 +23,12 @@ const config = {
   url: 'https://aharita.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/not-so-boring-guides/',
+  baseUrl: '/engineer-to-leader/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'aharita', // Usually your GitHub org/user name.
-  projectName: 'not-so-boring-guides', // Usually your repo name.
+  projectName: 'engineer-to-leader', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
